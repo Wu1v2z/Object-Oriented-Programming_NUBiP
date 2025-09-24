3899596d4b9368457dd2bbaaefdd737df6f4ee04
Wu1v2z <artyr.comfy3065@gmail.com>
2025-09-24 22:41:30 +0300

    Update Laboratorna_1_Kumanovski.cpp

--- Laboratorna_1_Kumanovski.cpp
+++ Laboratorna_1_Kumanovski.cpp
@@ -5,3 +5,20 @@ class Hourglass {
     double a; // first 
     double b; // second
 }
+
+public:
+
+    Hourglass() {
+        a = 0;
+        b = 0;
+        cout << "Конструктор без параметрів" << endl;   
+    }
+
+    Hourglass(double A; double B) {
+        a = A;
+        b = B;
+        cout << "Конструктор з параметрами" << end;
+    }   
+
+    
+
