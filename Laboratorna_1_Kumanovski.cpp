9b5f36b63132beb0beeb98de0ef68a42b85bb68b
Wu1v2z <artyr.comfy3065@gmail.com>
2025-10-27 21:23:11 +0200

    Update Laboratorna_1_Kumanovski.cpp

--- Laboratorna_1_Kumanovski.cpp
+++ Laboratorna_1_Kumanovski.cpp
@@ -6,58 +6,58 @@ class Hourglass {
     double b;
 
 public:
-    // Constructor without parameters
+    //Constructor without parameters
     Hourglass() {
         a = 0;
         b = 0;
         cout << "Конструктор без параметрiв викликаний" << endl;
     }
 
-    // Constructor with parameters
+    //Constructor with parameters
     Hourglass(double A, double B) {
         a = A;
         b = B;
         cout << "Конструктор з параметрами викликаний" << endl;
     }
 
-    // Destructor
+    //Destructor
     ~Hourglass() {
         cout << "Деструктор викликаний" << endl;
     }
 
-    // Methods set
+    //Methods set
     void setA(double A) { a = A; }
     void setB(double B) { b = B; }
 
-    // Methods get
+    //Methods get
     double getA() { return a; }
     double getB() { return b; }
 
-    // Method for calculating area (for example: area = a * b / 2)
+    //Method for calculating area (for example: area = a * b / 2)
     double area() {
         return (a * b) / 2.0;
     }
 
-    // Function print_sqrt()
+    //Function print_sqrt()
     void print_sqrt() {
         cout << "a = " << a << ", b = " << b
              << ", площа = " << area() << endl;
     }
 };
 
-// Type of pointer to a member function without parameters
+//Type of pointer to a member function without parameters
 typedef void (Hourglass::*FuncPtr)();
 
 int main() {
-    // Creating an object using the constructor without parameters
+    //Creating an object using the constructor without parameters
     Hourglass h1;
     h1.print_sqrt();
 
-    // Creating an object using a constructor with parameters
+    //Creating an object using a constructor with parameters
     Hourglass h2(4, 6);
     h2.print_sqrt();
 
-    // Використання покажчика на об’єкт
+    //Using a pointer to an object
     Hourglass *ptr = new Hourglass(10, 5);
     ptr->print_sqrt();
 
