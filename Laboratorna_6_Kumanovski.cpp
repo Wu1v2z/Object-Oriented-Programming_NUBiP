543e25307b05ea245fadd24facf320dff7bee48d
Wu1v2z <artyr.comfy3065@gmail.com>
2025-11-24 12:51:11 +0200

    Update Laboratorna_6_Kumanovski.cpp

--- Laboratorna_6_Kumanovski.cpp
+++ Laboratorna_6_Kumanovski.cpp
@@ -70,7 +70,7 @@ public:
     }
 };
 
-// ------------- ТУРБОРЕАКТИВНИЙ ДВИГУН ---------------
+// 
 class TurboJet : public Engine {
 private:
     double thrust;    // тяга в Ньютонах
@@ -93,7 +93,7 @@ public:
     }
 };
 
-// 
+// main функція
 int main() {
     setlocale(0, "Ukr");
 
