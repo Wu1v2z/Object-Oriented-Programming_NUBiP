012db3d4a62687da53bba1fe2f512b563e807713
Wu1v2z <artyr.comfy3065@gmail.com>
2025-11-28 10:35:37 +0200

    Update Laboratorna_7_Kumanovski.cpp

--- Laboratorna_7_Kumanovski.cpp
+++ Laboratorna_7_Kumanovski.cpp
@@ -4,4 +4,11 @@
 
 using namespace std;
 
-
+class GeometricFigure {
+public:
+    virtual double Area() const = 0;   // чисто віртуальна функція
+    virtual void Info() const {
+        cout << "Геометрична фігура" << endl;
+    }
+    virtual ~GeometricFigure() {}
+};
